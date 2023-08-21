7851e1e97b76befb54c311dc6110106501d36e93
rdubois-crypto <renaud.dubois@ledger.fr>
2023-08-21 14:57:21 +0200

    eddsa addition

---
+++ tests/WebAuthn_forge/test/FCL_eddsa.t.sol
@@ -0,0 +1,33 @@
+//********************************************************************************************/
+//  ___           _       ___               _         _    _ _
+// | __| _ ___ __| |_    / __|_ _ _  _ _ __| |_ ___  | |  (_) |__
+// | _| '_/ -_|_-< ' \  | (__| '_| || | '_ \  _/ _ \ | |__| | '_ \
+// |_||_| \___/__/_||_|  \___|_|  \_, | .__/\__\___/ |____|_|_.__/
+//                                |__/|_|
+///* Copyright (C) 2022 - Renaud Dubois - This file is part of FCL (Fresh CryptoLib) project
+///* License: This software is licensed under MIT License
+///* This Code may be reused including license and copyright notice.
+///* See LICENSE file at the root folder of the project.
+///* FILE: FCL_elliptic.t.sol
+///*
+///*
+///* DESCRIPTION: test file for ecdsa signature protocol
+///*
+//**************************************************************************************/
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+
+import "@solidity/FCL_eddsa.sol";
+
+
+contract EddsaTest is Test {
+  function test_add() public
+  {
+
+    
+  }
+
+
+}
