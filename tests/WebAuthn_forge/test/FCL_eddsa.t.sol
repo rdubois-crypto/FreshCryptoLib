05b7194f8954fdad3e4c82afce399dc07633921a
rdubois-crypto <renaud.dubois@ledger.fr>
2023-08-22 11:55:31 +0200

    mulmuladd

--- tests/WebAuthn_forge/test/FCL_eddsa.t.sol
+++ tests/WebAuthn_forge/test/FCL_eddsa.t.sol
@@ -22,5 +22,48 @@ import "forge-std/Test.sol";
 import "@solidity/FCL_eddsa.sol";
 
 contract EddsaTest is Test {
-    function test_add() public {}
+
+    function test_add() public {
+      
+        uint256 x;uint256 y;uint256 z;uint256 t;
+        uint256 x4;uint256 y4;uint256 z4;uint256 t4;
+        uint256 minus_gy=p-gy;//-gy
+
+        (x,y,z,t)=EDDSA.ed_Add(gx, gy,1,mulmod(gx,gy,p), gx, gy, 1, mulmod(gx,gy,p));//2G
+        (x4,y4,z4,t4)=EDDSA.ed_Add(x,y,z,t, x,y,z,t); //4G
+
+        (x4,y4,z4,t4)=EDDSA.ed_Add(x4,y4,z4,t4, gx,minus_gy,1,mulmod(gx,minus_gy,p)); //3G
+        (x4,y4,z4,t4)=EDDSA.ed_Add(x4,y4,z4,t4, gx,minus_gy,1,mulmod(gx,minus_gy,p)); //2G
+        (x4,y4)=EDDSA.ed_z2Aff(x4,y4,z4);
+        (x,y)=EDDSA.ed_z2Aff(x,y,z);
+
+      assertEq(x4,x);
+
+  assertEq(y4,y);
+
+    }
+
+    function test_dbl() public{
+         uint256 x;uint256 y;uint256 z;uint256 t;
+           uint256 x4;uint256 y4;uint256 z4;uint256 t4;
+      
+       (x,y,z,t)=EDDSA.ed_Add(gx, gy,1,mulmod(gx,gy,p), gx, gy, 1, mulmod(gx,gy,p));//2G
+       (x4,y4,z4,t4)=EDDSA.ed_Dbl(gx, gy,1); //2G
+ (x4,y4)=EDDSA.ed_z2Aff(x4,y4,z4);
+        (x,y)=EDDSA.ed_z2Aff(x,y,z);
+
+  assertEq(x4,x);
+
+  assertEq(y4,y);
+
+    }
+
+    function test_mulmuladd() public{
+      uint256 x_res;
+      //(n+1)G==G ?
+      x_res=EDDSA.ed_mulmuladd(gx,gy, 0x1000000000000000000000000000000014def9dea2f79cd65812631a5cf5d3ee, 0);
+      
+     // assertEq(x_res,gx); fail
+    }
 }
+
